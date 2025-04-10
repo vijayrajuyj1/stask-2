64ea3226aacad896ba258f5c44fdee0a5e0eec62
vijayarajult2 <vijayarajult2@gmail.com>
2025-04-10 13:37:47 +0530

    Add VPC + subnet module

---
+++ modules/networking/subnet.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "sub-1" {
+    vpc_id = aws_vpc.myvpc.id
+    availability_zone = "ap-southeast-1a"
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+
+    tags = {
+      name = "public-subnet"
+    }
+  
+}
---
+++ modules/networking/vpc.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    name="main-vpc"
+  }
+}
+
+
