1e49df08d5e881e9bd7c5b3c9a2872a86a802a6c
vijayarajult2 <vijayarajult2@gmail.com>
2025-04-10 14:53:11 +0530

    updated commit

--- main.tf
+++ main.tf
@@ -2,9 +2,16 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
+
+module "networking" {
+  source = "git::https://github.com/vijayrajuyj1/stask-2.git//modules/networking?ref=add-vpc-subnets"
+}
+
+
 resource "aws_instance" "web1" {
   ami = "ami-065a492fef70f84b1"
   instance_type = "t2.micro"
+  subnet_id = module.networking.public_subnet_id
 
   tags = {
     name="temp-ec2"
